4067f85d6f1ae00f266cc73565ed9083ed0943e9
Hrmn <singhharman2579@gmail.com>
2026-04-04 15:40:35 +0530

    Dataloading and cleaning done

---
+++ main.py
@@ -0,0 +1,25 @@
+# Data Loading
+
+import pandas as pd
+
+df = pd.read_csv("student_data.csv")
+
+print(df.head())
+
+# Data Cleaning
+
+print(df.isnull(). sum()) # for checking the missing values
+
+df.drop_duplicates(inplace = True) # for checking the duplicates
+
+df.fillna(0, inplace = True) # for filling the missing values
+
+
+
+
+
+
+
+
+
+
