e07e7bcdc6c07e5c9d771b8fdb225b69d062cd0e
Hrmn <singhharman2579@gmail.com>
2026-04-22 00:34:42 +0530

    done44

--- app.py
+++ app.py
@@ -4,10 +4,3 @@ import streamlit as st
 df = pd.read_csv("student_data.csv")
 st.dataframe(df)
 
-from src.main import performance 
-
-if df is not None:
-    df = performance(df)
-    
-    st.subheader("📊 Performance Data")
-    st.dataframe(df)
